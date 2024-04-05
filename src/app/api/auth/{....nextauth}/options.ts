6db3eb3e4a5817c650a1e4b9d35ef18b59cfc69e
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-04-05 01:54:16 +0100

    gitH

--- src/app/api/auth/{....nextauth}/options.ts
+++ src/app/api/auth/{....nextauth}/options.ts
@@ -1,5 +1,12 @@
 import type { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github"
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const options: NextAuthOptions {
-    providers: [],
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GitHub_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        })
+    ],
 }
