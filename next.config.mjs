eee284d832a47efc115eeee62291662ead48144c
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-04-09 23:45:41 +0100

    done with auth////

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         remotePatterns: [
             { 
                 protocol: 'https',
-                hostname: 'avatars.githubusercontent,com',
+                hostname: 'avatars.githubusercontent.com',
                 port: '',
                 pathname: '/u/**',
             }
